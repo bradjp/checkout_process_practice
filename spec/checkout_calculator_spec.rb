9c7d88bcf79d1549f88b609bb9671e033a4cc768
Brad Phillips <bjrp@protonmail.com>
2020-01-09 13:02:41 +0000

    Adds before each hook, adds and passes additional tests for illegal input, and for integer input

--- spec/checkout_calculator_spec.rb
+++ spec/checkout_calculator_spec.rb
@@ -1,10 +1,25 @@
 require 'checkout_calculator'
 
+
+
 describe '#checkout' do
+
+  before(:each) do
+    @shop = Shop.new
+  end
+
   context 'illegal input' do
     it 'returns -1 when passed an empty string' do
-      shop = Shop.new
-      expect(shop.checkout('')).to eq(-1)
+      expect(@shop.checkout('')).to eq(-1)
+    end
+    it 'returns -1 when passed a string containing lower case letters' do
+      expect(@shop.checkout('aBc')).to eq(-1)
+    end
+    it 'returns -1 when passed a string containing symbols and numbers' do
+      expect(@shop.checkout('-B8x')).to eq(-1)
+    end
+    it 'returns -1 when passed an integer' do
+      expect(@shop.checkout(18)).to eq(-1)
     end
   end
 end
